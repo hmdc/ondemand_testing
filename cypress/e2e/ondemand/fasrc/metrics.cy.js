42d02ea9c75e3d5a8e0af422e8c1c5740da2944d
David Verdú <davidverdu@theagilemonkeys.com>
2024-12-05 15:25:27 +0000

    create test for help section

---
+++ cypress/e2e/ondemand/fasrc/metrics.cy.js
@@ -0,0 +1,28 @@
+import { NAVIGATION, loadHomepage, navigateToMetrics } from "../../../support/utils/navigation.js";
+import {changeProfile} from "../../../support/utils/profiles";
+
+describe('FASRC Dashboard - Metrics Widget and Page', () => {
+  const fasrcClusterProfile = Cypress.env('fasrc_cluster_profile')
+  Cypress.config('baseUrl', NAVIGATION.baseUrl);
+
+  before(() => {
+    loadHomepage()
+    changeProfile(fasrcClusterProfile)
+  })
+
+  beforeEach(() => {
+    //DEFAULT SIZE FOR THESE TESTS
+    cy.viewport(cy.sid.screen.largeWidth, cy.sid.screen.height)
+    loadHomepage()
+  })
+
+  describe(`${fasrcClusterProfile}: Should display Metrics page`, () => {
+    navigateToMetrics()
+    
+    it('Help section', () => {
+      cy.get('div.metrics-help').should('be.visible')
+      cy.get('div.metrics-help div.introduction').should('be.visible')
+      cy.get('div.metrics-help div.description').should('be.visible')        
+    })
+  })
+})
