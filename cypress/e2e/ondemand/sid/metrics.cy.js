f8ef5bd19de550d34bae43afbab76d68b4e5357d
David Verdú <davidverdu@theagilemonkeys.com>
2024-12-09 14:29:25 +0000

    Metrics widget page tests (#3)
    * create test for help section
    
    * fix test
    
    * add dev.a-02 configuration
    
    * add test for lateral widgets
    
    * remove unneeded Makefile tasks
    
    * replicate tests for sid profile
    
    * change description texts
    
    * consider cluster in tests
    
    * add cluster_name to json properties
    
    * enhance tests
    
    * fix json and empty spaces

---
+++ cypress/e2e/ondemand/sid/metrics.cy.js
@@ -0,0 +1,44 @@
+import { NAVIGATION, loadHomepage, navigateToMetrics } from "../../../support/utils/navigation.js";
+import {changeProfile} from "../../../support/utils/profiles";
+
+describe('Sid Dashboard - Metrics Widget and Page', () => {
+  const fasrcClusterProfile = Cypress.env('fasrc_cluster_profile')
+  Cypress.config('baseUrl', NAVIGATION.baseUrl);
+  const clusterName = Cypress.env('cluster_name')
+
+  before(() => {
+    loadHomepage()    
+    changeProfile(cy.sid.profiles.sid.title)
+  })
+
+  beforeEach(() => {
+    //DEFAULT SIZE FOR THESE TESTS
+    cy.viewport(cy.sid.screen.largeWidth, cy.sid.screen.height)
+    loadHomepage()
+  })
+
+  describe('Should display Metrics page', () => {
+        
+    it('Help section', () => {
+      navigateToMetrics()
+      cy.get('div.metrics-help').should('be.visible')
+      cy.get('div.metrics-help').contains('h3','Metrics Help').should('be.visible')
+      cy.get('div.metrics-help div.introduction').should('be.visible')
+      cy.get('div.metrics-help div.description').should('be.visible') 
+      cy.get('div.metrics-help div.description').contains('h5','FairShare').should('be.visible') 
+      cy.get('div.metrics-help div.description').contains('h5','Completed Jobs by State').should('be.visible') 
+      cy.get('div.metrics-help div.description').contains('h5','Summary Job Stats').should('be.visible') 
+      cy.get('div.metrics-help div.description').contains('strong','Understanding Efficiency Metrics').should('be.visible')       
+    })
+
+    it('Widget section', () => {
+        navigateToMetrics()
+        cy.get('div.metrics').should('be.visible')
+        cy.get('div.metrics').contains('h3',`${clusterName} Cluster Metrics`).should('be.visible')
+        cy.get('div.metrics .metrics-widget-component .card .card-header').contains('h3','Fairshare').should('be.visible')
+        cy.get('div.metrics .metrics-widget-component .card .card-header').contains('h3','CPU Jobs by State').should('be.visible')
+        cy.get('div.metrics .metrics-widget-component .card .card-header').contains('h3','GPU Jobs by State').should('be.visible')
+        cy.get('div.metrics .metrics-widget-component .card .card-header').contains('h3','Summary Job Stats').should('be.visible')
+      })
+  })
+})
